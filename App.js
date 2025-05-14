7083c4fde8b9e2f47f4e005506fe68561c96c50b
ADRIANO TURRA VAZ <adriano.tvaz@senacsp.edu.br>
2025-05-14 19:26:10 -0300

    Primeiro commit de PoucaPanca

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={css.container}>
       <StatusBar style="auto" />
+      <Image source={require('./assets/logo_pouca_panca.png')} style={css.logo} />
+      <Text style={css.titulo}>Bem vindo ao Pouca Pança</Text>
+      <Text style={css.sub_titulo}>Alimentação</Text>
+      <Image source={require('./assets/cafe_da_manha.png')} style={css.cafe_da_manha} />
+      <Text style={css.refeicoes}>Café da Manhã</Text>
+      <Text style={css.item} >Item</Text>
+      <RNPickerSelect
+        onValueChange={(value) => console.log(value)}
+        items={[
+          { label: 'Maça', value: 'maca' },
+          { label: 'Banana', value: 'banana' },
+          { label: 'Ovo Frito', value: 'ovo_frito' },
+        ]}
+      />
+      <Text style={css.kcal} >Kcal</Text>
+      
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const css = StyleSheet.create({
+  logo:{flex:1, position:'absolute', width:60, height:60, marginTop:30, marginLeft:10},
+  titulo:{textAlign:'center', marginTop:25, fontSize:18},
+  sub_titulo:{textAlign:'center', marginTop:10, fontWeight:'bold', fontSize:15},
+  refeicoes:{fontWeight:'bold', marginLeft:90, paddingTop:35},
+  cafe_da_manha:{flex:1, position:'absolute', width:60, height:70, marginTop:90, marginLeft:15},
+  item:{fontWeight:'bold', color:'#A5B0BE', marginLeft:15, paddingTop:20, marginBottom:-10},
+  kcal:{fontWeight:'bold', color:'#A5B0BE', marginLeft:15,marginBottom:-10},
 });
