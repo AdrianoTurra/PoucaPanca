722845fd87910727f152dc7b58211f05ffef921a
ADRIANO TURRA VAZ <adriano.tvaz@senacsp.edu.br>
2025-05-16 22:00:05 -0300

    Atualizacao de sexta 2

--- App.js
+++ App.js
@@ -3,15 +3,18 @@ import { createStaticNavigation, useNavigation } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Resultados from './Resultados';
 import TelaInicial from './TelaInicial';
+import Perfil from './Perfil';
 
 const Abas = createBottomTabNavigator({
   screens: {
     Início: TelaInicial,
     Resultado: Resultados,
+    Perfil:Perfil,
   },
 });
 
 const Navigation = createStaticNavigation(Abas);
+
 
 export default function App() {
   return <Navigation />
